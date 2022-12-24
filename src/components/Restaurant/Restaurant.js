d21f9f34ce17d6862efad144747bdd12b5ec4d9f
Jasim Uddin <jasim.ice.iu@gmail.com>
2022-12-24 20:58:34 +0100

    localstorage and cart quantity fixed

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Meal from '../Meal/Meal';
 import OrderList from '../OrderList/OrderList';
 import './Restaurant.css';
@@ -14,11 +14,36 @@ const Restaurant = () => {
             .then(data => setMeals(data.meals));
     }, []);
 
+    useEffect(() => {
+        const storedOrder = getStoredCart();
+        const savedOrder = [];
+
+        for (const id in storedOrder) {
+            const addedMeal = meals.find(meal => meal.idMeal === id);
+            if (addedMeal) {
+                const quantity = storedOrder[id];
+                addedMeal.quantity = quantity;
+                savedOrder.push(addedMeal);
+            }
+        }
+        setOrders(savedOrder);
+    }, [meals])
+
     //add click handler
 
     const handleAddToOrder = meal => {
-        // console.log(meal);
-        const newOrders = [...orders, meal];
+        let newOrders = [];
+        const exist = orders.find(m => m.idMeal === meal.idMeal);
+        if (exist) {
+            const rest = orders.filter(m => m.idMeal !== meal.idMeal);
+            exist.quantity = exist.quantity + 1;
+            newOrders = [...rest, exist];
+        }
+        else {
+            meal.quantity = 1;
+            newOrders = [...orders, meal];
+
+        }
         setOrders(newOrders);
         addToDb(meal.idMeal);
     }
